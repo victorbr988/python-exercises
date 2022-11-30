07f89f83df7a5a812886254d2d4f25cc175df41c
Victor Gabriel <gabrielvghs666@gmail.com>
2022-11-30 15:44:40 -0300

    first exercise

---
+++ operators.py
@@ -0,0 +1,20 @@
+def sum (number1, number2):
+  return number1 + number2
+
+def sub (number1, number2):
+  return number1 - number2
+
+def mult (number1, number2):
+  return number1 * number2
+
+def potencial (number1, number2):
+  return number1 ** number2
+
+def div (number1, number2):
+  return number1 / number2
+
+print(sum(2,2))
+print(sub(2,2))
+print(mult(2,2))
+print(div(2,2))
+print(potencial(2,2))
